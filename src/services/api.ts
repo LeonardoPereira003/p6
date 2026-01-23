4f6219d9c83e561aea37c4f3ae90cceccdc18bba
Leonardo <reisleo619@gmail.com>
2026-01-23 00:07:55 -0300

    Ajustes

--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-// src/services/api.ts
-// Configuração central do Axios para a API Efood
-
-import axios from 'axios'
-
-export const api = axios.create({
-    baseURL: 'https://api-ebac.vercel.app/api/efood'
-})
--- src/types/Product.ts
+++
@@ -1,9 +0,0 @@
-// Tipagem de produto (API e Carrinho)
-export type Product = {
-    id: number
-    nome: string
-    descricao: string
-    foto: string
-    preco: number
-    porcao: string
-}
